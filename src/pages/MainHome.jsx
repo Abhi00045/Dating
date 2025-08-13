202338921e1adbb5c4b02e83740b1bd92a5e511a
abhi <rajidiabhinavreddy@gmail.com>
2025-08-14 02:36:05 +0530

    done badmosh menu bar

--- src/pages/MainHome.jsx
+++ src/pages/MainHome.jsx
@@ -1,9 +1,64 @@
+import { CiMenuBurger } from "react-icons/ci";
+import '../index.css'
+import "@fontsource/barriecito"; 
+import '@fontsource/abril-fatface';
+import { IoMdClose } from "react-icons/io";// defaults to weight 400
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const MainHome = ()=>{
+    const fullmenu = useRef(null);
+    const h1tag = useRef(null)
+    const menuclick = useRef(null)
+    const closeBut = useRef(null)
+
+
+    useGSAP(()=>{
+
+        const menuclicks = menuclick.current;
+        const closeButton = closeBut.current;
+
+        const tl = gsap.timeline();
+        tl.to("#fullmenu",{
+            right:0,
+            duration:0.5,
+        })
+        tl.from('#fullmenu h1',{
+            x:150,
+            duration:0.5,
+            stagger:0.2,
+            opacity:0
+        })
+        tl.from("#close",{
+            opacity:0,
+        })
+
+        tl.pause();
+        menuclicks.addEventListener("click" ,()=>{
+            tl.play();
+        })
+        closeButton.addEventListener("click" ,()=>{
+            tl.reverse();
+        })
+    },[])
 
     return(
         <>
-        <h1 className="text-5xl">Break the Ice Tonight</h1>
+        <div id="bodymain">
+            <div id="navBar">
+                <CiMenuBurger id="menu" ref={menuclick} size={36}
+                 />
+            </div>
+            <div id="fullmenu" ref={fullmenu} > 
+                  <h1 ref={h1tag}>Events</h1>
+                  <h1  ref={h1tag}>Gallery</h1>
+                  <h1  ref={h1tag}>Founders</h1>
+                  <h1  ref={h1tag}>Blog</h1>  
+
+                  <IoMdClose id="close" ref={closeBut} size={39} />
+            </div>
+        </div>
         </>
     )
 }
